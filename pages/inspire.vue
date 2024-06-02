b036a374406e34912f39f26ed2adc076db33d244
bazhenak <bazhena.kutergina@gmail.com>
2024-06-03 01:09:53 +0300

    init project

---
+++ pages/inspire.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import default2 from "~/layouts/default2.vue";
+
+export default {
+  name: 'InspirePage',
+  layout: 'default2'
+}
+</script>
